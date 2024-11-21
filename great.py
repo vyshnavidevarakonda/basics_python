a5285121b344eed314c7a4cb5ecbc35923870ddb
vyshnavidevarakonda <156453811+vyshnavidevarakonda@users.noreply.github.com>
2024-11-21 11:58:01 +0530

    Add files via upload

---
+++ great.py
@@ -0,0 +1,15 @@
+name = input()
+marks = int(input())
+if(marks>=90):
+    print("A grade")
+elif(80<=marks<=89):
+    print("B grade")
+elif(70<=marks<=79):
+    print("C grade")
+elif(60<=marks<=69):
+    print("D grade")
+else:
+    print("fail")
+
+
+
